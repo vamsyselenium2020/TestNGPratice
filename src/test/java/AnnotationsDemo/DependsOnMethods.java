a71f3610ccbca7323471135ffc5521337ea4d3b6
vamsyselenium2020 <vamsyselenium2020@gmail.com>
2020-01-26 21:17:03 +0530

    new chnages

--- src/test/java/AnnotationsDemo/DependsOnMethods.java
+++ src/test/java/AnnotationsDemo/DependsOnMethods.java
@@ -6,12 +6,12 @@ public class DependsOnMethods {
 
 	@Test
 	public void m1() {
-		System.out.println("hai from m1");
+		System.out.println("hai from m1 "+this.getClass().getName());
 	}
 	
 	@Test
 	public void m2() {
-		System.out.println("hai from m2");
+		System.out.println("hai from m2 "+this.getClass().getName());
 	}
 }
 
